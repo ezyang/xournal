c6fd9f450fc073d6a451bc8b7a102c7b99216dd8
Edward Z. Yang <ezyang@cs.stanford.edu>
2014-10-02 02:49:16 -0700

    ctrl+shift+D accelerator for Delete Page
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- src/xo-interface.c
+++ src/xo-interface.c
@@ -727,6 +727,9 @@ create_winMain (void)
   journalDeletePage = gtk_menu_item_new_with_mnemonic (_("_Delete Page"));
   gtk_widget_show (journalDeletePage);
   gtk_container_add (GTK_CONTAINER (menuJournal_menu), journalDeletePage);
+  gtk_widget_add_accelerator (journalDeletePage, "activate", accel_group,
+                              GDK_D, (GdkModifierType) GDK_CONTROL_MASK | GDK_SHIFT_MASK,
+                              GTK_ACCEL_VISIBLE);
 
   journalDuplicatePage = gtk_menu_item_new_with_mnemonic (_("_Duplicate Page"));
   gtk_widget_show (journalDuplicatePage);
